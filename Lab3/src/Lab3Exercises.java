879f14241f8e219ab3603cef5917c102f5bfa22c
4010e3 <4010e3@carambola.ece.uprm.edu>
2017-11-15 10:37:34 -0400

    Changed title to all caps.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Frederick");
+		JFrame myFrame = new JFrame("FREDERICK");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(850, 440);
 		myFrame.setSize(200, 200);
